981c2ae6fad94da673e51a4cc6dafc6b7631d791
Darkhorse31 <prateek1234569@gmail.com>
2022-06-08 23:18:34 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 import Main from "./Main";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Container/Login/Login";
-// import Registration from "./Container/Registration/Registration";
+import Registration from "./Container/Registration/Registration";
 import Shop from "../src/Container/Shop/Shop";
 import ShopIItems from "./Container/Shop/ShopItems/ShopIItems";
 import Forget from "./Container/Forget/Forget";
 import ShoppingCardSinglepage from "./Container/Shop/ShoppingCardSinglePage/ShoppingCardSinglepage";
-import Registration2 from "./Container/Registation2/Registration2";
 const LazyShop = React.lazy(() => import("../src/Container/Shop/Shop"));
 function App() {
   return (
     <div className="w-full">
       <Routes>
         <Route path="/" element={<Main />}>
+          <Route index element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
           <Route path="/forgetpassword" element={<Forget />} />
         </Route>
         <Route
