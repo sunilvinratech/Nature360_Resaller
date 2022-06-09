95df2ba10d02ae652ad994d0321830fa893783d3
VinraTech <80338843+VinraTech@users.noreply.github.com>
2022-06-09 09:31:15 +0530

    Done

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,8 @@
 import React from "react"
-import Navbar from "./Container/Navbar/Navbar";
 import {Outlet} from "react-router-dom"
 const Main=()=>{
     return <>
     <div className="w-full">
-        <Navbar/>
         <div className="w-full">
         <Outlet/>
         </div>
